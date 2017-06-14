865905fe9fe9a6c3c716436e82c05182e6b3fae4
Ivan Lavriv <Ivan.Lavriv@irco.com>
2017-06-14 19:20:48 +0300

    init presentation

---
+++ presentation/index.js
@@ -0,0 +1,66 @@
+// Import React
+import React from "react";
+
+// Import Spectacle Core tags
+import {
+  BlockQuote,
+  Cite,
+  Deck,
+  Heading,
+  ListItem,
+  List,
+  Quote,
+  Slide,
+  Link,
+  Text
+} from "spectacle";
+
+// Import image preloader util
+import preloader from "spectacle/lib/utils/preloader";
+
+// Import theme
+import createTheme from "spectacle/lib/themes/default";
+
+// Require CSS
+require("normalize.css");
+require("spectacle/lib/themes/default/index.css");
+
+
+const images = {
+  city: require("../assets/city.jpg"),
+  kat: require("../assets/kat.png"),
+  logo: require("../assets/formidable-logo.svg"),
+  markdown: require("../assets/markdown.png")
+};
+
+preloader(images);
+
+const theme = createTheme({
+  primary: "white",
+  secondary: "#1F2022",
+  tertiary: "#03A9FC",
+  quartenary: "#CECECE"
+}, {
+  primary: "Montserrat",
+  secondary: "Helvetica"
+});
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Continous integration in development
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={3} bold>
+            <Link href="http://lavriv92.github.io/ci-slides" target="_blank">lavriv92.github.io/ci-slides</Link>
+          </Text>
+        </Slide>
+        <Slide transition={["zoom", "slide"]} transitionDuration={500} >
+          <Heading >What is CI</Heading>
+        </Slide>
+      </Deck>
+    );
+  }
+}
